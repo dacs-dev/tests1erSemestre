dcd7b03926015b87ddcfbd8c31057e60b82c4d84
David Cid <davidcids99@gmail.com>
2025-01-12 02:07:36 +0100

    Se añade n de preguntas en el alert

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-function selectorTema(button){
+async function selectorTema(button){
     const container = button.closest('div[id')
     let location = ''
     console.log(button.id)
     console.log(container.id)
     const asignatura = container.id
     const tema = button.id
-    const preguntas = prompt("Numero de preguntas del test:");
+    let jsonFile = './'+asignatura+'/'+tema+'.json'
+    const tests = await fetch(jsonFile).then((response) => {
+        if (!response) {
+            throw new Error(`Error al leer el archivo: ${response.statusText}`);
+        }
+        return response.json().then((data) => {
+            return data
+        });
+    });
+    console.log(tests)
+    const preguntas = prompt(`Hay ${tests.length} preguntas, elije cuantas quieres en tu test:`);
     window.location.href = './test.html?asignatura='+asignatura+'&tema='+tema+'&preguntas='+preguntas
 }
